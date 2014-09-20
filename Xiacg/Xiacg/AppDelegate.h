d122ae023279e29297acf6dcb245afa80484517a
appale <appale@appaledeMac-mini.local>
2014-09-21 01:33:02 +0800

    Xiacg23

---
+++ Xiacg/Xiacg/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  Xiacg
+//
+//  Created by appale on 14-9-21.
+//  Copyright __MyCompanyName__ 2014年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
